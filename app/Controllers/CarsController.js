4307942eb886ad752dcca0e84c63b4b982fee885
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2021-03-31 14:25:13 -0600

    adds create

--- app/Controllers/CarsController.js
+++ app/Controllers/CarsController.js
@@ -1,9 +1,19 @@
 import { ProxyState } from "../AppState.js";
+import { carsService } from "../Services/CarsService.js";
 
 
 //Private
 function _draw() {
-
+  // What are we drawing
+  let cars = ProxyState.cars
+  let template = ''
+  // if a collection itterate over collection to generate template for each object
+  cars.forEach(car => {
+    console.log(car)
+    template += car.Template
+  })
+  // render to page
+  document.getElementById('cars').innerHTML = template
 }
 
 //Public
@@ -18,5 +28,42 @@ export default class CarsController {
     _draw()
   }
 
+
+  createCar() {
+    // if this method is triggered by an event (submit event) prevent the default action of reloding the page
+    window.event.preventDefault()
+    // grab the element from html that triggered this event
+    const form = window.event.target
+    debugger
+    let newCar = {
+      // @ts-ignore
+      make: form.make.value,
+      // @ts-ignore
+      model: form.model.value,
+      // @ts-ignore
+      year: form.year.value,
+      // @ts-ignore  this converts the string to a number
+      price: Number(form.price.value),
+      // @ts-ignore
+      description: form.description.value,
+      // @ts-ignore
+      imgUrl: form.imgUrl.value
+    }
+    carsService.createCar(newCar)
+
+    // @ts-ignore
+    form.reset()
+
+    // get the modal and close (using jQuery's "$" selector) 
+    $('#new-car-form').modal('hide')
+  }
+
+  deleteCar(id) {
+    carsService.deleteCar(id)
+  }
+
+  bid(id) {
+    carsService.bid(id)
+  }
 
 }
